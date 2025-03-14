a3b2f85ba8c51bbba9c615476552e04e1f1bf44b
Danyella Strikann <danyellastrikann@duck.com>
2025-03-14 16:51:57 +0100

    Init
    Signed-off-by: Danyella Strikann <danyellastrikann@duck.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import '@fontsource-variable/roboto';
+    import '$lib/app.scss';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="scss">
+    @use '$lib/_globals.scss';
+
+    h1 {
+        color: globals.$cl-text;
+    }
+</style>
