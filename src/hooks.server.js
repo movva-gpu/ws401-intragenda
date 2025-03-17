26fc228dd2eca16b7263352fc641a59256c118e7
Danyella Strikann <danyellastrikann@duck.com>
2025-03-17 09:26:50 +0100

    Emptied the thing
    Signed-off-by: Danyella Strikann <danyellastrikann@duck.com>

--- src/hooks.server.js
+++
@@ -1,7 +0,0 @@
-import { connect, createSchemas } from "$lib/db.js";
-
-/** @type {import('@sveltejs/kit').ServerInit} */
-export async function init() {
-    await connect();
-    await createSchemas()
-}
--- src/routes/+page.server.js
+++
@@ -1,8 +0,0 @@
-import { db } from "$lib/db.js";
-
-/** @type {import('@sveltejs/kit').Load} */
-export async function load() {
-    /** @type {App.User[]} */
-    const users = await db.execute("SELECT * FROM users;");
-    return { users };
-}
