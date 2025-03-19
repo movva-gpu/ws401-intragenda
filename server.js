e4aec91d093b4a6baf5edc649af3e758a91d85bf
Danyella Strikann <danyellastrikann@duck.com>
2025-03-19 12:49:02 +0100

    Debug 2

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import { createSecureServer } from 'http2';
 import { handler } from './build/handler.js';
 import { config } from 'dotenv'
 import fs from 'fs';
+import { createServer } from 'http';
 
 config();
 
 const options = {
-    key: fs.readFileSync(process.env.CERT_KEY),
-    cert: fs.readFileSync(process.env.CERT)
+    // key: fs.readFileSync(process.env.CERT_KEY),
+    // cert: fs.readFileSync(process.env.CERT)
 };
 
-const server = createSecureServer(options, handler);
+const server = createServer(options, handler);
 
 server.addListener('request', (res) => {
     console.log(res);
